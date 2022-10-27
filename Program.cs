ec498e1c621f5192cafcef03dd4a76e19b6d4b24
Tanner Kothlow <tannerkothlow@msn.com>
2022-10-27 18:23:46 -0500

    More variables established

--- Program.cs
+++ Program.cs
@@ -4,6 +4,23 @@ Console.WriteLine("Hello, World!");
 
 string? great = Console.ReadLine(); 
 
-int myNum = 5;
+int myNum;
+myNum = 5;
+
+double mcDouble = 4.99;
+
+bool isGreat = true;
 
 Console.WriteLine($"This is the {myNum} {great}");
+
+great = Console.ReadLine();
+
+Console.WriteLine($"This is still the {great} McDouble {mcDouble}");
+
+if (isGreat)
+{
+    Console.WriteLine("It's Great!");
+} else
+{
+    Console.WriteLine("Aw man :(");
+}
