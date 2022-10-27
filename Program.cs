3634ed4ce5dd6667cc76df8f25f22b8b1d627d77
Tanner Kothlow <tannerkothlow@msn.com>
2022-10-27 18:13:32 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+string? great = Console.ReadLine(); 
+
+int myNum = 5;
+
+Console.WriteLine($"This is the {myNum} {great}");
