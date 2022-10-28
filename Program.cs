96011717bdaf545120f65647440b7471526164d4
Tanner Kothlow <tannerkothlow@msn.com>
2022-10-27 19:28:43 -0500

    Fixed error CS8600 and played around with user input

--- Program.cs
+++ Program.cs
@@ -1,26 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-string? great = Console.ReadLine(); 
+// string? great = Console.ReadLine(); 
 
-int myNum;
-myNum = 5;
+// int myNum;
+// myNum = 5;
 
-double mcDouble = 4.99;
+// double mcDouble = 4.99;
 
-bool isGreat = true;
+bool isGreat = false;
 
-Console.WriteLine($"This is the {myNum} {great}");
+// Console.WriteLine($"This is the {myNum} {great}");
 
-great = Console.ReadLine();
+// great = Console.ReadLine();
 
-Console.WriteLine($"This is still the {great} McDouble {mcDouble}");
+// Console.WriteLine($"This is still the {great} McDouble {mcDouble}");
 
-if (isGreat)
-{
+Console.WriteLine("Is it great? (y/n)");
+string? isItGreat = Console.ReadLine();
+// This fixes that annoying warning I kept getting.
+if (isItGreat == null) {
+    isItGreat = "n";
+}
+
+// I was a fool for making isGreat and isItGreat different variables
+
+if (isItGreat.Length == 1) {
+    if (Convert.ToChar(isItGreat) == 'y' || Convert.ToChar(isItGreat) == 'Y') {
+        isGreat = true;
+    }
+} else if (isItGreat.Length >= 2) {
+    Console.WriteLine("Just enter y/n, do not type the answer out fully");
+}
+
+if (isGreat) {
     Console.WriteLine("It's Great!");
-} else
-{
+} else {
     Console.WriteLine("Aw man :(");
 }
